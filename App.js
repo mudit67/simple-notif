fe0f9dc4f2fc4e40aae8036a9885c51dfe12da80
mudit67 <bhardwajmudit006@gmail.com>
2022-03-29 15:53:04 +0530

    less 1

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Yo!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Yo!</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.boldText}>
+          <Text> Yo!</Text>
+          Lorem ipsum dolor sit amet consectetur
+        </Text>
+        <Text>
+          Lorem ipsum dolor sit amet consectetur
+        </Text>
+        <Text>
+          Lorem ipsum dolor sit amet consectetur
+        </Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +26,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    backgroundColor: "pink",
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: "bold",
+  },
+  body: {
+    backgroundColor: "yellow",
+    padding: 20,
+    fontWeight: "bold"
   },
 });
